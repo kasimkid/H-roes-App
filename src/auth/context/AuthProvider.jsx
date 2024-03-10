546aeb3acd77eaae35b42e4c902ca5544ae607c1
kasimkid <kasim85.ra@gmail.com>
2024-03-10 19:58:36 -0300

    Create user access

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { useReducer } from 'react'
 import { AuthContext } from './AuthContext'
 import { authReducer } from './authReducer'
+import { types } from '../types/types'
 
 
 const initialState = {
@@ -11,13 +12,14 @@ export const AuthProvider = ({ children }) => {
 
     const [ authState, dispatch ] = useReducer( authReducer, initialState )
 
-    const login = async ( name = ' ', types ) => {
+    const login = async ( name = ' ') => {
       const action = {
         types: types.login,
         payload: {
 
           id:'ABC',
           name: name
+          
         }
       }
 
